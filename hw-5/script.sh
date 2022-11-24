638ff00efb7ea995b0da57347d860955051bc1d0
PabloJuice <pablojuice.dev@gmail.com>
2022-11-24 20:07:19 +0100

    hw-5 fixes

--- hw-5/script.sh
+++ hw-5/script.sh
@@ -1,6 +1,6 @@
 mkdir /folder1
 mkdir /folder2
-mv /vagrant/folder-sync.service /lib/systemd/system/
+cp /vagrant/folder-sync.service /lib/systemd/system/
 systemctl daemon-reload
 systemctl start folder-sync
 systemctl enable folder-sync
